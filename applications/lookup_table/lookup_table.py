5a1c927204f06d5ef46dec300f3bcb5df70411eb
Aaron Beasley <aaroncb@aaroncb.com>
2020-06-03 21:14:03 -0500

    ADD Lookup Table
    - Added storing lookup tables for lines, x, y
    - Added reading files and printing the content to the console
    - Function runs sub 60 seconds currently

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,26 +1,40 @@
-# Your code here
+# import math
+# import random
 
+# f = open('lookup.txt', 'w')
+# f1 = open('x.txt', 'w')
+# f2 = open('y.txt', 'w')
 
-def slowfun_too_slow(x, y):
-    v = math.pow(x, y)
-    v = math.factorial(v)
-    v //= (x + y)
-    v %= 982451653
+# def slowfun_too_slow(x, y):
+#     v = math.pow(x, y)
+#     v = math.factorial(v)
+#     v //= (x + y)
+#     v %= 982451653
 
-    return v
+#     return v
 
-def slowfun(x, y):
-    """
-    Rewrite slowfun_too_slow() in here so that the program produces the same
-    output, but completes quickly instead of taking ages to run.
-    """
-    # Your code here
-
-
+# def slowfun(x, y):
+#     """
+#     Rewrite slowfun_too_slow() in here so that the program produces the same
+#     output, but completes quickly instead of taking ages to run.
+#     """
+#     v = (math.factorial(math.pow(x, y)) // (x+y)) % 982451653
+#     f.write(str(v)+'\n')
+#     f1.write(str(x)+'\n')
+#     f2.write(str(y)+'\n')
 
 # Do not modify below this line!
 
-for i in range(50000):
-    x = random.randrange(2, 14)
-    y = random.randrange(3, 6)
-    print(f'{i}: {x},{y}: {slowfun(x, y)}')
+# for i in range(50000):
+#     x = random.randrange(2, 14)
+#     y = random.randrange(3, 6)
+#     slowfun(x, y)
+
+lines = open('lookup.txt', 'r').readlines()
+x = open('x.txt', 'r').readlines();
+y = open('y.txt', 'r').readlines();
+
+count=0
+for line in lines:
+    print("{}: {},{}: {}".format(count, x[count].strip(), y[count].strip(), line.strip()))
+    count+=1
