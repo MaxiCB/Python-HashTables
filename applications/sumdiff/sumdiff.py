196356e463e98869cd1c98fac0bbdab27ef5ec38
Aaron Beasley <aaroncb@aaroncb.com>
2020-06-12 12:54:56 -0500

    ADD SumDiff
    - Completed sumdiff

--- applications/sumdiff/sumdiff.py
+++ applications/sumdiff/sumdiff.py
@@ -1,15 +1,25 @@
+import itertools
 """
 find all a, b, c, d in q such that
 f(a) + f(b) = f(c) - f(d)
 """
 
-#q = set(range(1, 10))
-#q = set(range(1, 200))
-q = (1, 3, 4, 7, 12)
+# q = set(range(1, 10))
+q = set(range(1, 200))
+# q = (1, 3, 4, 7, 12)
 
 
 def f(x):
     return x * 4 + 6
 
-# Your code here
 
+def sum_diff(a, b, c, d):
+    _a, _b, _c, _d = f(a), f(b), f(c), f(d)
+    if _a + _b == _c - _d:
+        print(f'f({a}) + f({b}) = f({c}) - f({d})  {_a} + {_b} = {_c} - {_d}')
+
+
+if __name__ == '__main__':
+    carts = list(itertools.product(q, repeat=4))
+    for _set in carts:
+        sum_diff(*_set)
