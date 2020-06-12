aac8bcc926a591aa0e3f1863a08ecbd402e0a6ac
Aaron Beasley <aaroncb@aaroncb.com>
2020-06-12 12:37:42 -0500

    ADD NoDupes
    - Added logic to no_dupes

--- applications/no_dupes/no_dupes.py
+++ applications/no_dupes/no_dupes.py
@@ -1,11 +1,19 @@
-def no_dups(s):
-    # Your code here
+def no_dupes(s):
+    # temp = s.split(" ")
+    # out_set = set(temp)
+    #
+    # return " ".join(out_set)
 
+    out = []
+    for i in s.split():
+        if s.count(i) > 1 and (i not in out) or s.count(i) is 1:
+            out.append(i)
+    return ' '.join(out)
 
 
 if __name__ == "__main__":
-    print(no_dups(""))
-    print(no_dups("hello"))
-    print(no_dups("hello hello"))
-    print(no_dups("cats dogs fish cats dogs"))
-    print(no_dups("spam spam spam eggs spam sausage spam spam and spam"))
+    print(no_dupes(""))
+    print(no_dupes("hello"))
+    print(no_dupes("hello hello"))
+    print(no_dupes("cats dogs fish cats dogs"))
+    print(no_dupes("spam spam spam eggs spam sausage spam spam and spam"))
